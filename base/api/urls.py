e5ae7ae215c1181d051243d35916f904c03e0bfa
akbermet1 <akbermet.zh@gmail.com>
2022-12-01 22:29:46 +0100

    customized the jwt token settings

--- base/api/urls.py
+++ base/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import getRoutes
+from .views import getRoutes, MyTokenObtainPairView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -9,6 +9,6 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("", getRoutes),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
