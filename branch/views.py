2010a646e7e4319cb5d19d5136dcf5a7f03a07af
Akbermet <akbermet.zh@gmail.com>
2023-01-04 16:08:58 +0100

    partial update endpoint initial ver

--- branch/views.py
+++ branch/views.py
@@ -37,3 +37,27 @@ class BranchInFirestoreListCreateView(APIView):
         list_of_branches = [branch.to_dict() for branch in all_branches]
 
         return Response(list_of_branches, status=status.HTTP_200_OK)
+
+
+class BranchInFirestorePartialUpdateView(APIView):
+    def post(self, request, branch_id):
+        address = request.data.get("address", None)
+        new_working_hours = request.data.get("new_working_hours", None)
+
+        if address is None and new_working_hours is None:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        branch_ref = db.collection(BRANCH_COLLECTION_ID).document(branch_id)
+
+        if branch_ref.get().exists:
+
+            if address is not None:
+                branch_ref.update(
+                    {
+                        "address": address
+                    }
+                )
+            return Response(branch_ref.get().to_dict(), status=status.HTTP_200_OK)
+
+        else:
+            return Response("Provided ID didn't match any branch!", status=status.HTTP_406_NOT_ACCEPTABLE)
