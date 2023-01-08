5c691d5474c3cbee45d6616b7b30b6a335de0631
Akbermet <akbermet.zh@gmail.com>
2023-01-08 17:34:24 +0100

    initial version of endpoint to add user to a queue

--- the_queue/urls.py
+++ the_queue/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path 
-from the_queue.views import (QueueInFirestoreListCreateView, QueueInFirestoreDetailView, QueueInFirestorePartialUpdateView)
+from the_queue.views import (QueueInFirestoreListCreateView, QueueInFirestoreDetailView, QueueInFirestorePartialUpdateView,
+                            CustomerInQueueCreateView)
 
 
 urlpatterns = [
     path("", QueueInFirestoreListCreateView.as_view(), name="queue-in-firebase-list-create"),
+    path("<str:queue_id>/customer/", CustomerInQueueCreateView.as_view(), name="customer-in-queue-list-create"),
     path("<str:queue_id>/", QueueInFirestoreDetailView.as_view(), name="queue-in-firebase-detail"),
     path("partial_update/<str:queue_id>/", QueueInFirestorePartialUpdateView.as_view(), name="queue-in-firebase-partial_update"),
 ]
--- the_queue/views.py
+++ the_queue/views.py
@@ -1,6 +1,10 @@
+import uuid
+from django.core.mail import send_mail
+from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from firebase_admin import firestore
 
 from electronic_queue.firestore import db
 from the_queue.serializers import QueueInFirebaseSerializer
@@ -87,3 +91,34 @@ class QueueInFirestorePartialUpdateView(APIView):
             return Response("Provided ID didn't match any queue!", status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
+class CustomerInQueueCreateView(APIView):
+    def post(self, request, queue_id):
+        recipients_email = request.data.get("recipients_email", None)
+
+        if recipients_email is not None and queue_id is not None:
+            queue_ref = db.collection(QUEUES_COLLECTION_ID).document(queue_id)
+
+            if not queue_ref.get().exists:
+                return Response("An invalid queue_id was provided.", status=status.HTTP_204_NO_CONTENT)
+
+            queue_doc = queue_ref.get().to_dict().get("name")
+            institution_id = queue_ref.get().to_dict().get("institution_id")
+            institution_doc = db.collection(INSTITUTIONS_COLLECTION_ID).document(institution_id).get().to_dict()
+            institution_name = institution_doc.get("name", None)
+            confirmation_code = str(uuid.uuid4())[:10]
+
+            queue_ref.update({
+                "queue": firestore.ArrayUnion([confirmation_code])
+                })
+
+            send_mail(
+                subject=f"Confirmation of reserving a place in {queue_doc} queue of {institution_name}",
+                message=f"Your confirmation code: {confirmation_code}",
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[recipients_email],
+                fail_silently=False
+            )
+            return Response("You have been added to the queue, and your confimation code has been sent to the email that you provided.", 
+                            status=status.HTTP_200_OK)
+        else:
+            return Response("Enter the email of a recepient and the ID of the queue!", status=status.HTTP_204_NO_CONTENT)
