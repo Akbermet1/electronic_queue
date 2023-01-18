ee18378f1130566fbfd6b62e4f9096ee112759c8
Akbermet <akbermet.zh@gmail.com>
2023-01-18 13:31:57 +0100

    initial ver of template to put customer in a queue

--- the_queue/urls.py
+++ the_queue/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path 
 from the_queue.views import (QueueInFirestoreListCreateView, QueueInFirestoreDetailView, QueueInFirestorePartialUpdateView,
-                            CustomerInQueueCreateView, CustomerInQueueDetailView, QueueInFirestoreMoveView, )
+                            CustomerInQueueCreateView, CustomerInQueueDetailView, QueueInFirestoreMoveView, 
+                            put_customer_in_queue_view, )
 
 
 urlpatterns = [
     path("", QueueInFirestoreListCreateView.as_view(), name="queue-in-firebase-list-create"),
-    path("<str:queue_id>/customer/", CustomerInQueueCreateView.as_view(), name="customer-in-queue-list-create"),
+    path("<str:queue_id>/customer/", put_customer_in_queue_view, name="put-customer-in-queue"),
+    # path("<str:queue_id>/customer/", CustomerInQueueCreateView.as_view(), name="customer-in-queue-list-create"),
     path("<str:queue_id>/customer/<str:confirmation_code>/", CustomerInQueueDetailView.as_view(), name="customer-in-queue-detail"),
     path("<str:queue_id>/move/", QueueInFirestoreMoveView.as_view(), name="queue-in-firebase-move"),
     path("<str:queue_id>/", QueueInFirestoreDetailView.as_view(), name="queue-in-firebase-detail"),
