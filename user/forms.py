3f8938440b85210bdba86a416bd3e77f0a43ce9e
Akbermet <akbermet.zh@gmail.com>
2023-10-04 00:11:04 +0200

    registration: institution gets created in firestore

--- user/forms.py
+++ user/forms.py
@@ -1,12 +1,27 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from electronic_queue.firestore import db
+from institution.views import INSTITUTIONS_COLLECTION_ID
 
 
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField()
     institution_name = forms.CharField()
+
+    def clean_institution_name(self):
+        institution_name = self.cleaned_data["institution_name"]
+        institutions_ref = db.collection(INSTITUTIONS_COLLECTION_ID)
+        docs_of_institutions = institutions_ref.where("name", "==", institution_name).stream()
+        all_docs = [doc.to_dict() for doc in docs_of_institutions]
+        if len(all_docs) > 0:
+            raise ValidationError(("This institution is already registered: %(value)s"),
+                                    code="invalid",
+                                    params={"value": institution_name},
+            )
+        return institution_name
 
     class Meta:
         model = User
--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
+import uuid
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from user.forms import RegisterUserForm
+from electronic_queue.firestore import db
+from institution.views import INSTITUTIONS_COLLECTION_ID
 
 
 def register_user_view(response):
@@ -8,6 +11,16 @@ def register_user_view(response):
         form = RegisterUserForm(response.POST)
         if form.is_valid():
             form.save()
+            institution_name = form.cleaned_data['institution_name']
+            institution_id = str(uuid.uuid4())[:10]
+            institution_ref = db.collection(INSTITUTIONS_COLLECTION_ID).document(institution_id)
+            institution_fields = {
+                "institution_id": institution_id,
+                "name": institution_name,
+                "branches": [],
+                "queues": [],
+            }
+            institution_ref.set(institution_fields)
             # change the name of the view that the user gets redirected to 
             return redirect("list-institutions")
     else:
