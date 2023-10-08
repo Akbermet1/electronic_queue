3029eccd5727f24b6ed9c0c5331ed4a231ad519e
Akbermet <akbermet.zh@gmail.com>
2023-10-08 22:08:34 +0200

    manage institution account: ver 1

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from user.views import register_user_view
+from user.views import register_user_view, manage_user_account_view
 
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("register/", register_user_view, name="register-user-view"),
+    path("manage_account/", manage_user_account_view, name="manage-user-account"),
 ]
--- user/views.py
+++ user/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from user.forms import RegisterUserForm
 from electronic_queue.firestore import db
-from institution.views import INSTITUTIONS_COLLECTION_ID
+from institution.views import INSTITUTIONS_COLLECTION_ID, BRANCH_COLLECTION_ID
+from django.contrib.auth.decorators import login_required
+
 
 
 def register_user_view(response):
@@ -29,4 +31,34 @@ def register_user_view(response):
         form = RegisterUserForm()
 
     return render(response, "./register/register.html", {"form": form}) 
+
+
+@login_required(login_url="/api/user/login/")
+def manage_user_account_view(request):
+    institution_email = request.user.email
+    institution_ref = db.collection(INSTITUTIONS_COLLECTION_ID)
+    institution_matches = institution_ref.where("email", "==", institution_email).stream()
+    all_institutions = [institution.to_dict() for institution in institution_matches]
+
+    institution_doc = {}
+    branches = []
+    true_match_found = False
+    if len(all_institutions) == 1:
+        institution_doc = all_institutions[0]
+        true_match_found = True
+
+        institution_id = institution_doc.get("institution_id")
+        branches_ref = db.collection(BRANCH_COLLECTION_ID)
+        docs_of_branches = branches_ref.where("institution_id", "==", institution_id).stream()
+        branches = [branch.to_dict() for branch in docs_of_branches]
+    
+    print(institution_doc)
+    context = {
+        "institution": institution_doc,
+        "branches": branches,
+        "true_match_found": true_match_found,
+    }
+    print(request.user)
+    print(request.user.email)
+    return render(request, "./institution/manage_account.html", context=context)
 
